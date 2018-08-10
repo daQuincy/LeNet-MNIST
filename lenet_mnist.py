34f13c6dd1e6b462044f9315f29da7e2d8fd52d3
daQuincy <yuquan95@gmail.com>
2018-08-10 23:27:31 +0800

    initial commit

---
+++ lenet_mnist.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 10 20:47:31 2018
+
+@author: YQ
+"""
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
+
+from keras.datasets import mnist
+
+
+def lenet(width, height, depth, classes):
+    inputShape = (height, width, depth)
+    model = Sequential()
+    
+    model.add(Conv2D(20, (5, 5), padding="same", input_shape=inputShape))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Conv2D(50, (5, 5), padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
+    model.add(Flatten())
+    model.add(Dense(500))
+    model.add(Activation("relu"))
+    
+    model.add(Dense(classes))
+    model.add(Activation("softmax"))
+    
+    return model
+    
+
+EPOCHS = 3
+LR = 1e-3
+BS = 64
+
+(Xtr, ytr), (Xte, yte) = mnist.load_data()
+
+Xtr = np.expand_dims(Xtr, -1)
+Xte = np.expand_dims(Xte, -1)
+
+Xtr = (Xtr / 255.0).astype(np.float32)
+Xte = (Xte / 255.0).astype(np.float32)
+
+ytr, yte = to_categorical(ytr, 10), to_categorical(yte, 10)
+
+model = lenet(28, 28, 1, 10)
+opt = Adam(lr=LR, decay=LR / EPOCHS)
+
+aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
+	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2, fill_mode="nearest")
+
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+H = model.fit_generator(aug.flow(Xtr, ytr, batch_size=BS),
+	validation_data=(Xte, yte), steps_per_epoch=len(Xtr) // BS,
+	epochs=EPOCHS, verbose=1)
+
+
+model.save("save/digits.h5")
---
+++ test.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 10 21:07:57 2018
+
+@author: YQ
+"""
+
+import cv2
+import numpy as np
+import imutils
+from keras.models import load_model
+from imutils import contours
+
+def preprocess(img, width, height):
+    padW = int((width - img.shape[1]) / 2.0)
+    padH = int((width - img.shape[0]) / 2.0)
+    
+    image = cv2.copyMakeBorder(img, padH, padH, padW, padW, cv2.BORDER_CONSTANT, value=0)
+    image = cv2.resize(image, (28, 28))
+    
+    return image
+    
+
+model = load_model("save/lenet_BS64_epoch3.h5")
+
+img = cv2.imread("test.png")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+thresh = cv2.dilate(thresh, cv2.getStructuringElement(cv2.MORPH_RECT,(5,5)))
+
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+cnts = contours.sort_contours(cnts)[0]
+
+for c in cnts:
+    (x, y, w, h) = cv2.boundingRect(c)
+    
+    out = thresh[y:y+h, x:x+w]
+    out = imutils.resize(out, height=20)
+    out = preprocess(out, 28, 28)
+    
+    show = out.copy()
+    
+    out = np.expand_dims(out, 0)
+    out = np.expand_dims(out, -1)
+    
+    sm = model.predict(out)
+    prediction = np.argmax(sm, axis=-1)
+    print(prediction)
+      
+    cv2.imshow("Out", show)
+    cv2.waitKey(0)
+    
+cv2.destroyAllWindows()
